d1d3b1a4fd9dcbac9ac569dd4888cebe76a70f20
Nelson Campos <ncamposfh@gmail.com>
2018-08-14 00:18:05 -0300

    todo

--- app.js
+++ app.js
@@ -2,26 +2,43 @@ const http = require('http');
 const buscaNaPasta = require('./busca-arquivos');
 const comparaArquivos = require('./comparadorDeArquivos');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 
 const srcFolder = './src/';
 const testFolder = './tests/';
 
+var src = buscaNaPasta.lerPastaSrc(srcFolder);
+var tests = buscaNaPasta.lerPastaTest(testFolder);
+var semTestes = comparaArquivos.comprarListas(src, tests);
+var html = montaBody(semTestes);
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(html);
+
 });
-
-var src = buscaNaPasta.lerPastaSrc(srcFolder);
-var tests = buscaNaPasta.lerPastaTest(testFolder);
-
-comparaArquivos.comprarListas(src, tests);
-
-
-
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function montaBody(semTestes) {
+  var header = '';
+
+  // TO-DO Gerar html
+  // var body = semTestes.forEach(item => 
+  //    "<li>"+item+"</li>"
+  // );
+
+  // var body = [];
+
+  // for (let i = 0; i < semTestes.length; i++) {
+  //   const element = semTestes[i];
+    
+  // }
+
+  return '<!DOCTYPE html>'+ '<html><head>' + header + '</head><body><ul>' + body + '+</ul></body></html>';
+};
+
 
